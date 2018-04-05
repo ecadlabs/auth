1d34bdc6d91825d0d78afc73785178c27a30a68c
Eugene Zagidullin <e.asphyx@gmail.com>
2018-04-05 19:23:17 +0300

    Test sql data #2

--- testdata.sql
+++ testdata.sql
@@ -34,8 +34,10 @@ CREATE TABLE users (
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO users VALUES ('0d4ef66e-8017-4c17-9b50-3a4ff263f6e8', 'e.asphyx@gmail.com', '$2a$10$jHWtDuzh4ZgVGOrorJ2F5uXSn5g9JlR40iAOwJtVRpLvlpqp5BnDq', 'Eugene', 'Zagidullin', '2018-03-29 02:06:10.849697', '2018-03-29 02:06:10.849697');
-INSERT INTO users VALUES ('14153e5c-c593-42f5-b606-1aabbdbc6f54', 'jev@ecadlabs.com', '$2a$10$C5qDedxsAJUa85DQEEor2.eqcSrjS3J5gmQNkS4X65hc1iOFgd4de', 'Jev', 'Björsell', '2018-03-29 02:09:26.333747', '2018-03-29 02:09:26.333747');
+-- pw: bee8Ohyi
+INSERT INTO users VALUES ('0d4ef66e-8017-4c17-9b50-3a4ff263f6e8', 'e.asphyx@gmail.com', '$2a$10$4myGRlgMSotMwZYzssK88utWPKMmiesQGC4/MQb.olm/OGcNrcCGW', 'Eugene', 'Zagidullin', '2018-03-29 02:06:10.849697', '2018-03-29 02:06:10.849697');
+-- pw: Aite4ad3
+INSERT INTO users VALUES ('14153e5c-c593-42f5-b606-1aabbdbc6f54', 'jev@ecadlabs.com', '$2a$10$Pue3Hipbh92TfkDKWS3xJ.Rt8OVWZ.NxlEQAKzuozTaOMEPRYw5UG', 'Jev', 'Björsell', '2018-03-29 02:09:26.333747', '2018-03-29 02:09:26.333747');
 
 
 --
