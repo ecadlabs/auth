efdf734a3a445a9424134e4bf73bf0b62743e31a
Eugene Zagidullin <e.asphyx@gmail.com>
2018-06-20 16:56:46 +0300

    Move token processing to middleware #7

---
+++ middleware/user_data.go
@@ -0,0 +1,61 @@
+package middleware
+
+import (
+	"context"
+	"git.ecadlabs.com/ecad/auth/handlers"
+	"git.ecadlabs.com/ecad/auth/users"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type UserData struct {
+	Namespace       string
+	TokenContextKey string
+	UserContextKey  string
+	DefaultRole     string
+}
+
+func (u *UserData) Handler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := users.User{
+			Roles: make(users.Roles),
+		}
+		req := r.WithContext(context.WithValue(r.Context(), u.UserContextKey, &user))
+
+		token, ok := r.Context().Value(u.TokenContextKey).(*jwt.Token)
+
+		if !ok {
+			user.Roles[u.DefaultRole] = struct{}{}
+			h.ServeHTTP(w, req)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		if names, ok := claims[nsClaim(u.Namespace, "roles")].([]interface{}); ok {
+			for _, name := range names {
+				if s, ok := name.(string); ok {
+					user.Roles[s] = struct{}{}
+				}
+			}
+		}
+
+		if len(user.Roles) == 0 {
+			user.Roles[u.DefaultRole] = struct{}{}
+		}
+
+		if sub, ok := claims["sub"].(string); ok {
+			if id, err := uuid.FromString(sub); err == nil {
+				user.ID = id
+				h.ServeHTTP(w, req)
+				return
+			} else {
+				log.Errorln(err)
+			}
+		}
+
+		handlers.JSONError(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+	})
+}
