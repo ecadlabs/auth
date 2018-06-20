26f3d8bea954cf418e73133dee51168e43914b89
Simon Boissonneault-Robert <simrob7000@gmail.com>
2018-06-20 10:04:52 -0400

    Added documentation tool compodoc

--- webapp/projects/ecad-angular-auth/src/lib/login/standard-login.service.ts
+++ webapp/projects/ecad-angular-auth/src/lib/login/standard-login.service.ts
@@ -13,6 +13,15 @@ import { ILoginService, Credentials, AuthConfig, LoginResult, User } from '../in
 export class StandardLoginService implements ILoginService {
 
   private readonly AUTO_REFRESH_INTERVAL = (this.config && this.config.autoRefreshInterval) || 60000;
+  public user: BehaviorSubject<User> = new BehaviorSubject(this.token);
+
+  public isLoggedIn: Observable<Boolean> = this.user.pipe(
+    map(() => {
+      const rawToken = localStorage.getItem(this.config.tokenName) || null;
+      return !!(rawToken && !this.jwtHelper.isTokenExpired(rawToken));
+    }),
+    distinctUntilChanged()
+  );
 
   constructor(
     @Optional()
@@ -32,11 +41,11 @@ export class StandardLoginService implements ILoginService {
         return interval(this.AUTO_REFRESH_INTERVAL)
         .pipe(switchMap(() => {
           return this.refreshToken().pipe(catchError(() => observableOf(false)));
-        }))
+        }));
       }),
       tap(() => this.user.next(this.token))
     )
-    .subscribe()
+    .subscribe();
   }
 
   private createRequestOptions(credential: Credentials) {
@@ -58,8 +67,9 @@ export class StandardLoginService implements ILoginService {
     }
   }
 
-  public user: BehaviorSubject<User> = new BehaviorSubject(this.token);
-
+  /**
+  * Authenticate the user with basic auth using loginUrl provided in authConfig
+  */
   public login(credential: Credentials): Observable<LoginResult> {
     const requestOptions = this.createRequestOptions(credential);
     return this.httpClient.get<LoginResult>(this.config.loginUrl, requestOptions).pipe(
@@ -69,12 +79,18 @@ export class StandardLoginService implements ILoginService {
     );
   }
 
+  /*
+  * Check if the ip is whitelisted by querying the whiteListUrl provided in authConfig
+  */
   public get isIpWhiteListed(): Observable<Boolean> {
     return this.httpClient.get(this.config.whiteListUrl, { observe: 'response' }).pipe(
       map((response) => String(response.status) === '200'),
       catchError((err, response) => observableOf(false)));
   }
 
+  /*
+  * Logout the user by removing his JWT from local storage
+  */
   public logout(): Observable<Boolean> {
     return Observable.create((observer: Observer<Boolean>) => {
       localStorage.setItem(this.config.tokenName, '');
@@ -83,15 +99,10 @@ export class StandardLoginService implements ILoginService {
     });
   }
 
+  /*
+  * Refresh the JWT by querying the refresh url provided in previous login response
+  */
   public refreshToken(): Observable<boolean> {
     return this.httpClient.get(localStorage.getItem('refreshTokenUrl')).pipe(map(() => true));
   }
-
-  public isLoggedIn: Observable<Boolean> = this.user.pipe(
-    map(() => {
-      const rawToken = localStorage.getItem(this.config.tokenName) || null;
-      return !!(rawToken && !this.jwtHelper.isTokenExpired(rawToken));
-    }),
-    distinctUntilChanged()
-  );
 }
