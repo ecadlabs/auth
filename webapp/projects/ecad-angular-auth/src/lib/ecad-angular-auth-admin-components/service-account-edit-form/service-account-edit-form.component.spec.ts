e546206a8ec8e00dc648029dfb9e4c39a9bc87ad
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-04-15 14:04:19 -0400

    Fix tests

--- webapp/projects/ecad-angular-auth/src/lib/ecad-angular-auth-admin-components/service-account-edit-form/service-account-edit-form.component.spec.ts
+++ webapp/projects/ecad-angular-auth/src/lib/ecad-angular-auth-admin-components/service-account-edit-form/service-account-edit-form.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServiceAccountEditFormComponent } from './service-account-edit-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef
+} from '@angular/material';
+import { USERS_SERVICE } from '../../ecad-angular-auth-admin/tokens';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ServiceAccountEditFormComponent', () => {
   let component: ServiceAccountEditFormComponent;
@@ -8,9 +17,24 @@ describe('ServiceAccountEditFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServiceAccountEditFormComponent ]
-    })
-    .compileComponents();
+      declarations: [ServiceAccountEditFormComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatInputModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: null },
+        {
+          provide: USERS_SERVICE,
+          useValue: {
+            getRoles: () => [{ value: 'user', displayValue: 'User' }]
+          }
+        },
+        { provide: MatDialogRef, useValue: {} }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
