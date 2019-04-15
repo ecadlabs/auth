aa41a5b9c59adcb345219ad317da25c69986c2e5
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-04-15 15:43:16 -0400

    Fix linting errors

--- webapp/projects/ecad-angular-auth/src/lib/ecad-angular-auth/login/standard-login.service.ts
+++ webapp/projects/ecad-angular-auth/src/lib/ecad-angular-auth/login/standard-login.service.ts
@@ -43,7 +43,7 @@ export class StandardLoginService implements ILoginService {
       tap(result => localStorage.setItem('refreshTokenUrl', result.refresh)),
       tap(() => this.user.next(this.getTokenAndCheckExp()))
     );
-  };
+  }
 
   constructor(
     @Optional()
